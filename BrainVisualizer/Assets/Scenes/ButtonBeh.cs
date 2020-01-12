f526d15758bcc097c1dfae489ab4918e32f1888c
Tal <tal.eingar@gmail.com>
2020-01-13 00:38:36 +0200

    Adding a sample button

---
+++ BrainVisualizer/Assets/Scenes/ButtonBeh.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBeh : MonoBehaviour
+{
+    public UnityEngine.UI.Text txt;
+    bool buttonStatus;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonStatus = false;
+        txt.text = "Init";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnButtonPress()
+    {
+        Debug.Log("pressed");
+        buttonStatus = !buttonStatus;
+        if (buttonStatus)
+        {
+            txt.text = "Pressed";
+        }
+        else
+        {
+            txt.text = "Not pressed";
+        }
+    }
+}

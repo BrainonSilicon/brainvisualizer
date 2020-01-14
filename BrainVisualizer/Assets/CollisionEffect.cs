5d0bf449834332c5134bee5d42d9eb9d1f212f61
Tal <tal.eingar@gmail.com>
2020-01-14 10:52:34 +0200

    Trying to add colliders

---
+++ BrainVisualizer/Assets/CollisionEffect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionEffect : MonoBehaviour
+{
+ //   public UnityEngine.ParticleSystem particleSys;
+ //   public UnityEngine.Color color;
+ //   public UnityEngine.ParticleSystem.MinMaxGradient color1;
+
+    public void Start()
+    {
+   //     var c = particleSys.colorOverLifetime;
+  //      c.color = color;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        transform.localScale /= 1.3f;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        transform.localScale *= 1.3f;
+    }
+}
---
+++ BrainVisualizer/Assets/Mover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float speed = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var currentPos = transform.position;
+        currentPos.x += speed * Time.deltaTime;
+
+        if (currentPos.x > 0.5f || currentPos.x < -0.5f)
+        {
+            speed *= -1;
+        }
+    }
+}

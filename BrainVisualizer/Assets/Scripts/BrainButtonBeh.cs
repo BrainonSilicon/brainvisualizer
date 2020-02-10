0f5e0554d680a17608010b30ae3936dc26561d3c
Tal <tal.eingar@gmail.com>
2020-02-10 23:14:39 +0200

    new stuff

---
+++ BrainVisualizer/Assets/Scripts/BrainButtonBeh.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrainButtonBeh : MonoBehaviour
+{
+    public GameObject object1;
+    public GameObject object2;
+
+    private bool buttonStatus = false;
+
+    public void OnPress()
+    {
+        buttonStatus = !buttonStatus;
+
+        object1.SetActive(buttonStatus);
+        object2.SetActive(!buttonStatus);
+    }
+}

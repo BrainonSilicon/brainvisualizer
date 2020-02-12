fbb5edc4d0daa0a748b4e0e5989ce4a9073fcb6e
Tal <tal.eingar@gmail.com>
2020-02-12 14:01:11 +0200

    adding recenter button to brain scene

---
+++ BrainVisualizer/Assets/Scripts/recenter.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class recenter : MonoBehaviour
+{
+    public GameObject baseObject;
+    public float distance;
+
+    public void changePosition()
+    {
+        transform.position = baseObject.transform.position + baseObject.transform.forward * distance;
+    }
+}
